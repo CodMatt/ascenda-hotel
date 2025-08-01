51cead2362a15662a0d56936d24950251e6d689b
Faustina <faustinachong@gmail.com>
2025-08-01 19:11:42 +0800

    vite config- added vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom', // 👈 This line is crucial
+    globals: true,
+    setupFiles: './src/test/setup.ts', // optional but good for global mocks
+  }
 })
